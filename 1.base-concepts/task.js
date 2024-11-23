cdc06bab90f738d56f61a2d5772973a64fe3e3e3
Oganes <og.marckaryan@yandex.ru>
2024-11-23 20:59:53 +0300

    Решил задание 1, задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  const D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    return [];
+  } else if (D == 0) {
+    arr[0] = -b / (2 * a);
+    return arr;
+  } else {
+    const x1 = (-b + Math.sqrt(D)) / (2 * a);
+    const x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr[0] = x1;
+    arr[1] = x2;
+    return arr;
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
