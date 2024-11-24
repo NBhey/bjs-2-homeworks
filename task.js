f0513a471e0960d20feb20379582191041d0fcf1
NBhey <122431282+NBhey@users.noreply.github.com>
2024-11-24 16:21:38 +0000

    Deploying to gh-pages from @ NBhey/bjs-2-homeworks@8b89411a59165dde0eb453e5ee349d18874ffe08 🚀

---
+++ task.js
@@ -0,0 +1,25 @@
+"use strict";
+function solveEquation(a, b, c) {
+  let arr = [];
+  const D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    return [];
+  } else if (D == 0) {
+    arr[0] = -b / (2 * a);
+    return arr;
+  } else {
+    const x1 = (-b + Math.sqrt(D)) / (2 * a);
+    const x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr[0] = x1;
+    arr[1] = x2;
+    return arr;
+  }
+}
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  percent = (percent * 0.01) / 12;
+  const bodyCredit = amount - contribution;
+  const pay =
+    bodyCredit * (percent + percent / ((1 + percent) ** countMonths - 1));
+  return Number((pay * countMonths).toFixed(2));
+}
