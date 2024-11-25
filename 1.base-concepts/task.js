f130241673897eefb5cfb5c45e8b1b9e88382bab
Oganes <og.marckaryan@yandex.ru>
2024-11-25 19:34:50 +0300

    поправил функцию solveEquation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,18 +2,15 @@
 function solveEquation(a, b, c) {
   let arr = [];
   const D = b ** 2 - 4 * a * c;
-  if (D < 0) {
-    return [];
-  } else if (D == 0) {
-    arr[0] = -b / (2 * a);
-    return arr;
-  } else {
+  if (D > 0) {
     const x1 = (-b + Math.sqrt(D)) / (2 * a);
     const x2 = (-b - Math.sqrt(D)) / (2 * a);
     arr[0] = x1;
     arr[1] = x2;
-    return arr;
+  } else if (D == 0) {
+    arr[0] = -b / (2 * a);
   }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
