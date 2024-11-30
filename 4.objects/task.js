67d782b24fd7ff0f0ae19b5d0d49ff01629df8f4
Oganes <og.marckaryan@yandex.ru>
2024-11-30 18:22:52 +0300

    4 Задание, третья задача выпонлил

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -14,14 +14,9 @@ Student.prototype.addMarks = function (...marks) {
 };
 
 Student.prototype.getAverage = function () {
-  console.log(`1ый  ${this.marks}`);
-  if (this.marks === undefined || this.marks.length === 0 ) return 0;
-  console.log(`2ой  ${this.marks}`)
-  return this.marks.reduce((acc,el)=>{return acc+=el},0) / this.marks.length
+  
 };
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+
 };
