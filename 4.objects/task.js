cf84cb71c0b1456239475caf58551aa5bd3e7b0c
Oganes <og.marckaryan@yandex.ru>
2024-11-30 17:31:50 +0300

    4 Задание, первая задача выпонлил

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,21 @@
 function Student(name, gender, age) {
-  
+  return {
+    name,
+    gender,
+    age,
+    marks: [],
+  };
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    const obj = {
+        subjectName
+    }
+  return obj.subjectName
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marks) {};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function () {};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function (reason) {};
