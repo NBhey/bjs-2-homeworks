be50e12904a762efc92aef16b9840530c6d0e2d3
Oganes <og.marckaryan@yandex.ru>
2024-11-30 18:20:05 +0300

    4 Задание, вторая задача выпонлил

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,21 +1,27 @@
 function Student(name, gender, age) {
-  return {
-    name,
-    gender,
-    age,
-    marks: [],
-  };
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-    const obj = {
-        subjectName
-    }
-  return obj.subjectName
+  this.subject = subjectName;
 };
 
-Student.prototype.addMarks = function (...marks) {};
+Student.prototype.addMarks = function (...marks) {
+  this.marks === undefined ? undefined : this.marks.push(...marks);
+};
 
-Student.prototype.getAverage = function () {};
+Student.prototype.getAverage = function () {
+  console.log(`1ый  ${this.marks}`);
+  if (this.marks === undefined || this.marks.length === 0 ) return 0;
+  console.log(`2ой  ${this.marks}`)
+  return this.marks.reduce((acc,el)=>{return acc+=el},0) / this.marks.length
+};
 
-Student.prototype.exclude = function (reason) {};
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
