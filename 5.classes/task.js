51fe596e6b2c087c0f886aeae40d078f0456ec66
Oganes <og.marckaryan@yandex.ru>
2025-01-07 16:09:51 +0300

    Начал выполнение пятого задания

---
+++ 5.classes/task.js
@@ -0,0 +1,13 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+ fix(){
+    this.state = this.state * 1.5
+ }
+ set this.state = 1
+}
