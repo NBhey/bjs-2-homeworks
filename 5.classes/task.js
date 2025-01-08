c6a56896e44615f49ba8356e98bf8f95f26e43bf
Oganes <og.marckaryan@yandex.ru>
2025-01-08 19:56:54 +0300

    Задание 5, задача 1 выполнил

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -6,8 +6,20 @@ class PrintEditionItem {
     this.state = 100;
     this.type = null;
   }
- fix(){
-    this.state = this.state * 1.5
- }
- set this.state = 1
+  fix() {
+    this.state = this.state * 1.5;
+  }
+  set state(number) {
+    if (number < 0) {
+      this._state = 0;
+    } else if (number > 100) {
+      this._state = 100
+    } else {
+      this._state = number
+    }
+  }
+
+  get state(){
+   return this._state
+  }
 }
